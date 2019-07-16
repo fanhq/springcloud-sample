d1c2d884ffecffa504779cc09c081e97a63dfd2b
fanhaiqiu <fanhaiqiu@cmiot.chinamobile.com>
2019-07-16 14:45:01 +0800

    springcloud-feign

--- springcloud-zookeeper/src/main/java/com/fanhq/example/ZookeeperApplication.java
+++ springcloud-zookeeper/src/main/java/com/fanhq/example/ZookeeperApplication.java
@@ -3,13 +3,11 @@ package com.fanhq.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Configuration;
 
 /**
  * @author fanhaiqiu
  * @date 2019/7/15
  */
-@Configuration
 @EnableAutoConfiguration
 @EnableDiscoveryClient
 public class ZookeeperApplication {
