69b955cb32dd1249e480f302b5a4b9bcb11aa998
fanhaiqiu <fanhaiqiu@cmiot.chinamobile.com>
2019-07-16 14:33:21 +0800

    springcloud-feign

--- springcloud-feign/src/main/java/com/fanhq/example/client/OpenClient.java
+++ springcloud-feign/src/main/java/com/fanhq/example/client/OpenClient.java
@@ -1,8 +1,18 @@
 package com.fanhq.example.client;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * @author fanhaiqiu
  * @date 2019/7/15
  */
+
+@Component
+@FeignClient(value="server")
 public interface OpenClient {
+
+    @RequestMapping(value = "/show")
+    public String hi();
 }
--- springcloud-feign/src/main/java/com/fanhq/example/consumer/ConsumeController.java
+++ springcloud-feign/src/main/java/com/fanhq/example/consumer/ConsumeController.java
@@ -1,5 +1,7 @@
 package com.fanhq.example.consumer;
 
+import com.fanhq.example.client.OpenClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,11 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2019/7/15
  */
 @RestController
-@RequestMapping(value = "open")
-public class ConsumerController {
+@RequestMapping(value = "consume")
+public class ConsumeController {
+
+    @Autowired
+    private OpenClient openClient;
 
     @RequestMapping("/hi")
     public String hi() {
-        return "Hello World";
+        return openClient.hi();
     }
 }
